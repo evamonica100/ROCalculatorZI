f52610c2e85f5246d87c27cf1a3de69fee2828d4
evamonica100 <eva.monica02@gmail.com>
2026-01-11 11:53:23 +0700

    Update ROCalculator.tsx

--- app/components/ROCalculator.tsx
+++ app/components/ROCalculator.tsx
@@ -827,7 +827,7 @@ const feedOsmoticPressure = initialFeedOsmoticPressure * concentrationRatio;
 // --------------------------------------------
 const maxElementRecovery = elementType.includes('SW') ? 0.12 : 0.15;
 const elementRecovery =
-  Math.min(maxElementRecovery, 0.5 * targetRecovery / elementsInStage);
+  Math.min(maxElementRecovery, 0.5 * targetRecovery / elementsByStage);
 element.recovery = elementRecovery;
 
 // --------------------------------------------
